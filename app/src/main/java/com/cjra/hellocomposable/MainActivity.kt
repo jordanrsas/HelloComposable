42205ba4482a61713bd622b36bc802947965df8a
Jordan Rosas <cj.rosasavila@gmail.com>
2024-04-16 20:07:46 -0600

    Lecture 3.33 Modifiers

--- app/src/main/java/com/cjra/hellocomposable/MainActivity.kt
+++ app/src/main/java/com/cjra/hellocomposable/MainActivity.kt
@@ -3,57 +3,50 @@ package com.cjra.hellocomposable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.cjra.hellocomposable.ui.theme.HelloComposableTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloComposableTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Content()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Button(onClick = { hello() }) {
-        Text(text = name)
-    }
-}
-
-@Preview
-@Composable
-fun MyButton() {
-    Button(onClick = { println("Hello, how are you?") }) {
-        Text(text = "My button")
-    }
-}
-
-fun hello() {
-    println("Hello JetPack Compose")
-}
-
+/*
+* Modifiers
+* -Position and Size: width, heigth
+* -Function: click, scroll
+* -Appearance: background, padding, border
+* -listeners: events> onKeyEvent
+* */
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HelloComposableTheme {
-        Greeting("Android")
-    }
+fun Content() {
+    Text(
+        text = "Hello Jetpack Componse",
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(horizontal = 30.dp)
+            .clickable {
+                println("Hello Jetpack")
+            }
+    )
 }
